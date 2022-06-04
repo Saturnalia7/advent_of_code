68b9140bae87e70752f8fe97c349fdb4afadde55
colby-miller <comiller@protonmail.com>
2022-06-04 18:04:09 -0500

    day 5 part 1 solution

--- 2021/05/main.py
+++ 2021/05/main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def read_data():
     with open("input.txt", "r") as f:
         data = [line.strip() for line in f.readlines()]
@@ -5,40 +8,27 @@ def read_data():
     return data
 
 
-def get_points(data: list):
-    points = []
-    for _set in data:
-        _set = _set.split(" -> ")
-        set_conv = []
-        for point in _set:
-            set_conv.append([int(val) for val in point.split(",")])
-        points.append(set_conv)
-    return points
+def part1(data):
+    allPoints = []
+    for line in data:
+        # Parse data into usable contents
+        firstPoint, secondPoint = line.split("->")
+        x1, y1 = tuple(map(int, firstPoint.split(",")))
+        x2, y2 = tuple(map(int, secondPoint.split(",")))
 
+        # Consider vertical & horizontal lines only
+        if x1 == x2 or y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                for y in range(min(y1, y2), max(y1, y2) + 1):
+                    allPoints.append((x, y))
 
-def iter_list_max(data: list, _max=None):
-    if _max is None:
-        _max = float("-inf")
-    for el in data:
-        if isinstance(el, list):
-            _max = iter_list_max(el, _max)
-        else:
-            _max = max(el, _max)
-    return _max
-
-
-def part1(points: list):
-    result = 0
-    _max = iter_list_max(points)
-    print(_max)
-    return result
+    return len([point for point in Counter(allPoints).values() if point > 1])
 
 
 def main():
     data = read_data()
-    points = get_points(data=data)
 
-    print(part1(points))
+    print(part1(data))
 
 
 if __name__ == "__main__":
